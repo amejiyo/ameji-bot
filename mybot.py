40c2b06bc6f709c48109ec07a8d50fa703311a87
amejiyo <44494218+amejiyo@users.noreply.github.com>
2018-10-27 17:33:31 +0700

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -10,12 +10,47 @@ from discord.voice_client import VoiceClient
 
 BOT_PREFIX = ("/")
 
+
 client = Bot(command_prefix=BOT_PREFIX)
 
 newUserDMMessage = "Welcome to LOUDHOUSE"
 
 
 #voice command
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('/hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    elif message.content.startswith('/pretty'):
+        await client.send_message(message.channel, "Ameji is beautiful girl")
+    if message.content.startswith('/yemo'):
+        await client.send_message(message.channel, 'The old man?')
+        msg = await client.wait_for_message(author=message.author, content='oops')
+        await client.send_message(message.channel, ';P')
+    if message.content.startswith('/rule'):
+        msg = "Faction rules: \n 1. Prob and audit only target faction or serve faction. \n2. Weekly credit must be more than 1000 per week. \n3. Respond to higher-ups at 7:30-8:30. \n4. Don't  be offline continuously 3 days (or more than). \nIf you are busy or want any excuse contact elites, home secretary, deputy, diplomat \n\nThe punishment \nkick"
+        await client.send_message(message.channel, msg)
+    elif message.content.startswith('/bug'):
+        await client.send_message(message.channel, 'Please contact ameji')
+
+    if message.content.startswith('/cool'):
+        await client.send_message(message.channel, 'Who is cool? Type @name')
+        def check(msg):
+            return msg.content.startswith('{0.author.mention}'.format(message))
+        message = await client.wait_for_message(author=message.author, check=check)
+        await client.send_message(message.channel, '{0.author.mention} is cool indeed'.format(message))
+
+@client.event
+async def on_member_join(member):
+    server = member.server
+    fmt = 'Welcome {0.mention} to {1.name}!'
+    await client.send_message(server, fmt.format(member, server))
 
 #8ball game
 @client.command(name= '8ball',
@@ -86,5 +121,5 @@ async def list_servers():
 
 client.loop.create_task(list_servers())
 
-
-bot.run(os.environ['BOT_TOKEN'])
+client.run(TOKEN)
+bot.run(TOKEN)
